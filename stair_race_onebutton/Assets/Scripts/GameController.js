331e839915c52fb8c67c4653f18de9c3723ce482
sunwangshu <sunwangshu1993@gmail.com>
2016-04-28 20:21:30 -0400

    one side & one button

---
+++ stair_race_onebutton/Assets/Scripts/GameController.js
@@ -0,0 +1,56 @@
+﻿#pragma strict
+
+var startKey1: KeyCode;
+var startKey2: KeyCode;
+var startKey3: KeyCode;
+var startKey4: KeyCode;
+
+var endheight: float = 16;	//whoever first reach the height is the winner
+
+var stage: int = 1;		//1: gameplay; 2: end game;
+
+var player1: Player;
+var player2: Player;
+
+var result: UI.Text;	// result
+var timeText: UI.Text;	// result
+var timeCount: float = 0.0;
+var timeStart: float;
+
+function Start () {
+	result.text = "";
+	timeText.text = "Press any button to start racing!";
+}
+
+function Update () {
+	// start
+	if (stage == 1) {
+		if(Input.GetKeyDown(startKey1) || Input.GetKeyDown(startKey2) || Input.GetKeyDown(startKey3) ||Input.GetKeyDown(startKey4)){
+			player1.startGame();
+			player2.startGame();
+			timeCount = 0.0;
+			timeStart = Time.time;
+			stage = 2;
+		}
+	}
+
+	if (stage == 2) {
+		timeCount = Time.time - timeStart;
+		timeText.text = "Time: " + timeCount;
+		if(player1.transform.position.y >= endheight) {
+			// player1 wins
+			result.text = "<- Sun wins the star!";
+			//result.text.color = #AA0000FF;
+			player1.endGame();
+			player2.endGame();
+			stage = 0;
+		}
+		else if (player2.transform.position.y >= endheight) {
+			// player2 wins
+			result.text = "-> Moon wins the star!";
+			player1.endGame();
+			player2.endGame();
+			stage = 0;
+		}
+	}
+}
---
+++ stair_race_onebutton/Assets/Scripts/Player.js
@@ -0,0 +1,42 @@
+﻿#pragma strict
+
+var playing: boolean = false;
+var star: GameObject;
+
+var key1 : KeyCode;
+//var key2: KeyCode;
+//var key1Once: boolean = false;
+//var key2Once: boolean = false;
+
+var stepx: float = 0.35;
+var stepy: float = 0.35;
+
+function Start () {
+	
+}
+
+function Update () {
+	if (playing){
+//		if (Input.GetKeyDown(key1)) {
+//			key1Once = true;
+//		}
+//		if (Input.GetKeyDown(key2)) {
+//			key2Once = true;
+//		}
+
+		if (Input.GetKeyDown(key1)){
+			transform.position.x += stepx;
+			transform.position.y += stepy;
+//			key1Once = false;
+//			key2Once = false;
+		}
+	}
+}
+
+public function startGame () {
+	playing = true;
+}
+
+function endGame () {
+	playing = false;
+}
