0deefb62e8c784e2f3237887e34cf67ecc93321c
kevings <kgs239@nyu.edu>
2016-04-30 00:07:59 -0400

    Added both players
    Both players now in the game, D and E for player 1, L and O for player 2
    to move

---
+++ stair_race_fallingfire - Kevins version/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (player != null)
+        {
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+        }
+        else
+        {
+            Debug.Log("Player not set correctly. Please check if tag is set correctly.");
+        }
+    }
+}
--- stair_race_fallingfire - Kevins version/Assets/Scripts/Player2.cs
+++ stair_race_fallingfire - Kevins version/Assets/Scripts/Player2.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player1 : MonoBehaviour
+public class Player2 : MonoBehaviour
 {
     //[SerializeField] private Texture[] textures;
     [HideInInspector] public bool jump = false;
-    public float moveForce = 10f;
-    public float maxSpeed = 10f;
-    public float jumpForce = 300f;
+    public float moveForce2 = 10f;
+    public float maxSpeed2 = 10f;
+    public float jumpForce2 = 300f;
 
-    public bool grounded;
+    public bool grounded2;
 
     private Rigidbody2D rb;
     
@@ -21,23 +21,23 @@ public class Player1 : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButtonDown("Player1Up") && grounded)
+        if (Input.GetButtonDown("Player2Up") && grounded2)
         {
-            rb.AddForce(new Vector2(0f, jumpForce));
-            grounded = false;
+            rb.AddForce(new Vector2(0f, jumpForce2));
+            grounded2 = false;
         }
     }
 
     void FixedUpdate()
     {
-        float h = Input.GetAxis("Player1Right");
-        if (h * rb.velocity.x < maxSpeed)
+        float h = Input.GetAxis("Player2Right");
+        if (h * rb.velocity.x < maxSpeed2)
         {
-            rb.AddForce(Vector2.right * h * moveForce);
+            rb.AddForce(Vector2.right * h * moveForce2);
         }
-        if (Mathf.Abs(rb.velocity.x) > maxSpeed)
+        if (Mathf.Abs(rb.velocity.x) > maxSpeed2)
         {
-            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed2, rb.velocity.y);
         }
     }
 
@@ -45,7 +45,7 @@ public class Player1 : MonoBehaviour
     {
         if (col.gameObject.tag == "Stairs")
         {
-            grounded = true;
+            grounded2 = true;
         }
     }
 
